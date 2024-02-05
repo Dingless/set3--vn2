75da974120165d930f74851b7e91ac8fae9eda30
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-02-05 12:39:04 +0100

    gjort övn 2 hade massor problem men fick till de till slut glömde int

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Skapa en klass 
+public class Kalkylator
+{
+    // Metod som tar in en integer som parameter och skriver ut kvadraten av talet
+    public void PrintSquare(int number)
+    {
+        int square = number * number; // Beräkna kvadraten av det angivna talet
+        Console.WriteLine("Kvadraten av " + number + " är " + square); // Skriv ut kvadraten
+    }
+
+    // startpunkt
+    public static void Main(string[] args)
+    {
+        Kalkylator kalkylator = new Kalkylator();
+        kalkylator.PrintSquare(5); // print square metoden använder nummret 5
+    }
+}
